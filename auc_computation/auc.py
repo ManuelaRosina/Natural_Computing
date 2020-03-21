fixed auc computation

auc_computation/auc.py
@@ -8,7 +8,7 @@ def compute_score(y, y2, pred1, pred2):
 
     return roc_auc_score(y_true, scores)
 
-def plot_results(y, y2, pred1, pred2)
+def plot_results(y, y2, pred1, pred2):
     y_true, scores = merge_lists(y, y2, pred1, pred2)
     fpr, tpr, thresholds = roc_curve(y_true, scores)
     plt.plot(fpr, tpr)
@@ -17,28 +17,28 @@ def plot_results(y, y2, pred1, pred2)
     plt.ylabel("True Positive Rate")
     plt.show()
 
-def merge_lists(y, y2, pred1, pred2)
+def merge_lists(y, y2, pred1, pred2):
     y_true = np.array(y + y2)
     scores = np.concatenate((pred1,pred2))
 
     return y_true, scores
 
 # load the two text files with the scores
-outfile = '.test6.out'
+outfile = '.test.out6'
 english_pred = np.loadtxt('english'+outfile)
-tagalog_pred = np.loadtxt('tagalog'+outfile)
-hiligaynon_pred = np.loadtxt('hiligaynon'+outfile)
-middle_english_pred = np.loadtxt('middle-english'+outfile)
-plautdietsch_pred = np.loadtxt('plautdietsch'+outfile)
-xhosa_pred = np.loadtxt('xhosa'+outfile)
+#tagalog_pred = np.loadtxt('tagalog'+outfile)
+#hiligaynon_pred = np.loadtxt('hiligaynon'+outfile)
+#middle_english_pred = np.loadtxt('middle-english'+outfile)
+#plautdietsch_pred = np.loadtxt('plautdietsch'+outfile)
+#xhosa_pred = np.loadtxt('xhosa'+outfile)
 
 # assign the labels
-y = [1 for x in english_pred]
-y2 = [0 for x in tagalog_pred]
-y3 = [0 for x in hiligaynon_pred]
-y4 = [0 for x in middle_english_pred]
-y5 = [0 for x in plautdietsch_pred]
-y6 = [0 for x in xhosa_pred]
+y = [0 for x in english_pred]
+y2 = [1 for x in tagalog_pred]
+#y3 = [1 for x in hiligaynon_pred]
+#y4 = [1 for x in middle_english_pred]
+#y5 = [1 for x in plautdietsch_pred]
+#y6 = [1 for x in xhosa_pred]
 
 # plot curve
 plot_results(y, y2, english_pred, tagalog_pred)
@@ -46,11 +46,11 @@ plot_results(y, y2, english_pred, tagalog_pred)
 # compute and print the score
 print('tagalog:')
 print(compute_score(y, y2, english_pred, tagalog_pred))
-print('hiligaynon:')
-print(compute_score(y, y3, english_pred, hiligaynon_pred))
-print('middle-english:')
-print(compute_score(y, y4, english_pred, middle_english_pred))
-print('plautdietsch:')
-print(compute_score(y, y5, english_pred, plautdietsch_pred))
-print('xhosa:')
-print(compute_score(y, y6, english_pred, xhosa_pred))
+#print('hiligaynon:')
+#print(compute_score(y, y3, english_pred, hiligaynon_pred))
+#print('middle-english:')
+#print(compute_score(y, y4, english_pred, middle_english_pred))
+#print('plautdietsch:')
+#print(compute_score(y, y5, english_pred, plautdietsch_pred))
+#print('xhosa:')
+#print(compute_score(y, y6, english_pred, xhosa_pred))

negative-selection/auc.py
@@ -18,12 +18,12 @@ plautdietsch_pred = np.loadtxt(folder+'plautdietsch'+outfile)
 xhosa_pred = np.loadtxt(folder+'xhosa'+outfile)
 
 # assign the labels
-y = [1 for x in english_pred]
-y2 = [0 for x in tagalog_pred]
-y3 = [0 for x in hiligaynon_pred]
-y4 = [0 for x in middle_english_pred]
-y5 = [0 for x in plautdietsch_pred]
-y6 = [0 for x in xhosa_pred]
+y = [0 for x in english_pred]
+y2 = [1 for x in tagalog_pred]
+y3 = [1 for x in hiligaynon_pred]
+y4 = [1 for x in middle_english_pred]
+y5 = [1 for x in plautdietsch_pred]
+y6 = [1 for x in xhosa_pred]
 
 # compute and print the score
 print('tagalog:')