ae11b045c76981533426ff4b5ee6e5dcf0b3d3a2
Manuela <manuela.bergau@gmail.com>
2020-03-21 10:22:59 +0100

    skript update

--- auc_computation/auc.py
+++ auc_computation/auc.py
@@ -1,17 +1,37 @@
 import numpy as np
 from sklearn.metrics import roc_auc_score
 
+def compute_score(y, y2, pred1, pred2):
+    y_true = np.array(y + y2)
+    scores = np.concatenate((pred1,pred2))
+
+    return roc_auc_score(y_true, scores)
+
 # load the two text files with the scores
-english_pred = np.loadtxt('english.test.out')
-tagalog_pred = np.loadtxt('tagalog.test.out')
+outfile = '.test6.out'
+english_pred = np.loadtxt('english'+outfile)
+tagalog_pred = np.loadtxt('tagalog'+outfile)
+hiligaynon_pred = np.loadtxt('hiligaynon'+outfile)
+middle_english_pred = np.loadtxt('middle-english'+outfile)
+plautdietsch_pred = np.loadtxt('plautdietsch'+outfile)
+xhosa_pred = np.loadtxt('xhosa'+outfile)
 
 # assign the labels
 y = [1 for x in english_pred]
 y2 = [0 for x in tagalog_pred]
-
-# merge lists to one
-y_true = np.array(y + y2)
-scores = np.concatenate((english_pred,tagalog_pred))
+y3 = [0 for x in hiligaynon_pred]
+y4 = [0 for x in middle_english_pred]
+y5 = [0 for x in plautdietsch_pred]
+y6 = [0 for x in xhosa_pred]
 
 # compute and print the score
-print(roc_auc_score(y_true, scores))
+print('tagalog:')
+print(compute_score(y, y2, english_pred, tagalog_pred))
+print('hiligaynon:')
+print(compute_score(y, y3, english_pred, hiligaynon_pred))
+print('middle-english:')
+print(compute_score(y, y4, english_pred, middle_english_pred))
+print('plautdietsch:')
+print(compute_score(y, y5, english_pred, plautdietsch_pred))
+print('xhosa:')
+print(compute_score(y, y6, english_pred, xhosa_pred))
